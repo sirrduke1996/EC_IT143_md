1ff0188b9418de6519643f513eef90713041d3fd
Mustapha B. Dukuly <mustaphabdukuly22@gmail.com>
2026-02-13 14:07:14 +0000

    Add files via upload
    Adding deliverable W6.4

---
+++ W6.4_Performance_Analysis.sql
@@ -0,0 +1,3 @@
+SELECT BusinessEntityID, FirstName, LastName
+FROM Person.Person
+WHERE FirstName = 'John';
---
+++ W6.4_Performance_Analysis.sql3.sql
@@ -0,0 +1,2 @@
+CREATE NONCLUSTERED INDEX IX_Person_FirstName
+ON Person.Person (FirstName);
